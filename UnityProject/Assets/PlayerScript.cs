76e4da5dece242c3a7be24d22a96f8dc6336d831
Tim Schoonman <root@is1337.eu>
2014-04-14 14:12:19 +0200

    finally. also fixed gitignore

---
+++ UnityProject/Assets/PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+    float movementSpeed = 5.0f;
+    float verticalMoveAmount = 0.0f;
+    float horizontalMoveAmount = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //verticalMoveAmount = Input.GetAxis("Vertical") * movementSpeed;
+        //horizontalMoveAmount = Input.GetAxis("Horizontal") * movementSpeed;
+        //verticalMoveAmount *= Time.deltaTime;
+        //horizontalMoveAmount *= Time.deltaTime;
+        //transform.Translate(horizontalMoveAmount, verticalMoveAmount, 0);
+    }
+
+    void FixedUpdate()
+    {
+        
+        rigidbody2D.velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidbody2D.AddForce(new Vector2(verticalMoveAmount, 0));
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rigidbody2D.AddForce(new Vector2(verticalMoveAmount, 0));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody2D.AddForce(new Vector2(0, horizontalMoveAmount));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody2D.AddForce(new Vector2(0, horizontalMoveAmount));
+        }
+    }
+}
