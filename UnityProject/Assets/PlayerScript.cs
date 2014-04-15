83c8ac0cd74311720a972d6408f20d470d3f84bb
Tim Schoonman <root@is1337.eu>
2014-04-15 13:39:33 +0200

    manually reconfiguring project

---
+++ UnityProject/Assets/PlayerScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerScript : MonoBehaviour
+{
+    float movementSpeed = 5000.0f;//TODO: figure out better value.
+    public float movementMultiplier = 100.0f;
+    float verticalMoveAmount = 0.0f;
+    float horizontalMoveAmount = 0.0f;
+
+    void OnTriggerEnter()
+    {
+    }
+
+    void OnCollisionEnter(Collider col)
+    {
+        
+    }
+
+    void OnGUI()
+    {
+
+    }
+
+    void Update()
+    {
+        verticalMoveAmount = Input.GetAxis("Vertical") * movementSpeed * movementMultiplier;
+        horizontalMoveAmount = Input.GetAxis("Horizontal") * movementSpeed * movementMultiplier;
+    }
+
+    void FixedUpdate()
+    {
+
+        rigidbody2D.velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidbody2D.AddForce(new Vector2(0, verticalMoveAmount));
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigidbody2D.AddForce(new Vector2(0, verticalMoveAmount));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody2D.AddForce(new Vector2(horizontalMoveAmount, 0));
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody2D.AddForce(new Vector2(horizontalMoveAmount, 0));
+        }
+    }
+}
